ed78bfe6dd24c97449ec03ab24e5f8774bf51105
unknown <sahil.qaiser@gmail.com>
2020-05-15 18:33:40 +0530

    basic init

---
+++ res/script.js
@@ -0,0 +1,11 @@
+$interval = 2000;
+setInterval(()=> {
+    if($('#name').css('display') == 'none')
+    {
+        $('#name').css('display','inline-block');
+        $('#name-urdu').css('display','none');
+    } else {
+        $('#name-urdu').css('display','inline-block');
+        $('#name').css('display','none');
+    }
+},$interval);
